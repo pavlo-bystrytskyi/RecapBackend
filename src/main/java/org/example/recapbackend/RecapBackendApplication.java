e7703994222b51c687bc809a8704f6ec179e41f0
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-10-01 09:15:21 +0200

    Initial commit.

---
+++ src/main/java/org/example/recapbackend/RecapBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.recapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/recapbackend/RecapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
